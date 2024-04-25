3c11d3d82f0d6cf12b6e8844e514a8a4bb2bccb2
Yagavi Kannadhasan <yagavi1994@gmail.com>
2024-04-25 21:02:19 +0000

    Make seperate functions for name, zodiac_1 and zodiac_2 inputs

--- run.py
+++ run.py
@@ -1,50 +1,72 @@
+#Import necessary modules
+#The basic concept of the code is learnt from "https://www.geeksforgeeks.org/how-to-check-horoscope-using-python/"
 import requests 
 from bs4 import BeautifulSoup 
 
-def horoscope(zodiac_sign: int, day: int) -> str: 
+def horoscope(zodiac_sign: int, day: int) -> str:
+    """
+    Pass the user input variables to the website.
+    """ 
     url = ( 
         "https://www.horoscope.com/us/horoscopes/general/"
         f"horoscope-general-daily-{day}.aspx?sign={zodiac_sign}"
     ) 
+
+    # Soup will collect all the data from the website
     soup = BeautifulSoup(requests.get(url).content, "html.parser") 
+
+    # Search for main-horoscope class from the HTML of the website's code and return it.
     return soup.find("div", class_="main-horoscope").p.text 
 
 
-def get_zodiac_input():
+def introduction():
+    """
+    Gets user input of their name
+    """
 
     print("\nWelcome to 'Your Horoscope' \n")
 
+    name = input("Please enter your name: \n")
+
+    return name
+
+
+def get_zodiac_1(input):
+
+    # Zodiac dictionary to get zodiac_sign input to pass in horoscope function.
     zodiac_dict = {
         '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, 
         '7': 7, '8': 8, '9': 9, '10': 10, '11': 11, '12': 12
     }
 
+    
+    zodiac_1 = zodiac_dict[input]
+
+    return zodiac_1
+
+    # if validate_data_for_zodiac(zodiac_sign):
+    #     pass
+
+    # else:
+    #     return None
+
+def get_zodiac_2(input):
+    # Zodiac dictionary to use for string literals
     zodiac_dict_2 = {
-        '1': 'Aries', '2': 'Taurus', '3': 'Gemini', '4': 'Cancer', '5': 'Leo', '6': 'Virgo', '7': 'Libra',
-        '8': 'Scorpio', '9': 'Sagittarius', '10': 'Capricorn', '11': 'Aquarius', '12': 'Pisces'
-    }
-    
-    
-    print('\nChoose the number of your zodiac sign from below list : \n',
-        "\n1. Aries (Mar 21 - Apr 19) \n", "\n2. Taurus (Apr 20 - May 20) \n", 
-        "\n3. Gemini (May 21 - Jun 20) \n", "\n4. Cancer (Jun 21 - Jul 22)\n", 
-        "\n5. Leo (Jul 23 - Aug 22) \n", "\n6. Virgo (Aug 23 - Sep 22) \n", 
-        "\n7. Libra (Sep 23 - Oct 22) \n", "\n8. Scorpio (Oct 23 - Nov 21) \n", 
-        "\n9. Sagittarius (Nov 22 - Dec 21) \n", "\n10. Capricorn (Dec 22 - Jan 19) \n", 
-        "\n11. Aquarius (Jan 20 - Feb 18) \n", "\n12. Pisces (Feb 19 - Mar 20)\n")
+            '1': 'Aries', '2': 'Taurus', '3': 'Gemini', '4': 'Cancer', '5': 'Leo', '6': 'Virgo', '7': 'Libra',
+            '8': 'Scorpio', '9': 'Sagittarius', '10': 'Capricorn', '11': 'Aquarius', '12': 'Pisces'
+        }
 
-    name = input("Please enter your name: \n")
-    
-    zodiac_sign = input("\nInput your zodiac sign number: \n")
+    zodiac_2 = zodiac_dict_2[input]
 
-    zodiac_1 = zodiac_dict[zodiac_sign]
+    return zodiac_2
 
-    zodiac_2 = zodiac_dict_2[zodiac_sign]
-
-    return name, zodiac_1, zodiac_2
 
 
 def get_day_input():
+    """
+    Gets user input of day 
+    """
 
     day_dict = {
         '1': "yesterday", '2': "today", '3': "tomorrow"
@@ -56,15 +78,31 @@ def get_day_input():
     day_input = input("Input the number of the day: \n")
     print("\n")
 
-    if validate_data(day_input):
+    if validate_data_for_day(day_input):
         day = day_dict[day_input]
-        print(f"You have selected {day}")
         return day
     else:
         return None
 
+def validate_data_for_zodiac(value):
+    """
+    Inside the try, checks whether the value is integer.
+    Raises ValueError if input cannot be converted into int,
+    or if the value is not between 1 to 3.
+    """
+    try:
+        [int(value)]
+        if 1 <= int(value) <= 12:
+            return True
+        else:
+            raise ValueError
 
-def validate_data(value):
+    except ValueError:
+        print(f"Invalid data: Enter a number between 1 to 12, please try again.\n")
+        return False
+
+
+def validate_data_for_day(value):
     """
     Inside the try, checks whether the value is integer.
     Raises ValueError if input cannot be converted into int,
@@ -75,26 +113,44 @@ def validate_data(value):
         if 1 <= int(value) <= 3:
             return True
         else:
-            raise ValueError(
-                f"Enter a number between 1 to 3, you provided {value}"
-            )
-    except ValueError as e:
-        print(f"Invalid data: {e}, please try again.\n")
+            raise ValueError
+
+    except ValueError:
+        print(f"Invalid data: Enter a number between 1 to 3, please try again.\n")
         return False
 
 
 
 def main():
     """
-    Run all functions
+    Runs all functions
     """
-    inputs = get_zodiac_input()
+
+    name = introduction()
+
+    print('\nChoose the number of your zodiac sign from below list : \n',
+        "\n1. Aries (Mar 21 - Apr 19) \n", "\n2. Taurus (Apr 20 - May 20) \n", 
+        "\n3. Gemini (May 21 - Jun 20) \n", "\n4. Cancer (Jun 21 - Jul 22)\n", 
+        "\n5. Leo (Jul 23 - Aug 22) \n", "\n6. Virgo (Aug 23 - Sep 22) \n", 
+        "\n7. Libra (Sep 23 - Oct 22) \n", "\n8. Scorpio (Oct 23 - Nov 21) \n", 
+        "\n9. Sagittarius (Nov 22 - Dec 21) \n", "\n10. Capricorn (Dec 22 - Jan 19) \n", 
+        "\n11. Aquarius (Jan 20 - Feb 18) \n", "\n12. Pisces (Feb 19 - Mar 20)\n")
+
+    zodiac_sign = input("\nInput your zodiac sign number: \n")
+    
+    zodiac_1 = get_zodiac_1(zodiac_sign)
+
+    zodiac_2 = get_zodiac_2(zodiac_sign)
+        
     day = None
-    while day is None:
+    # Loops if invalid data is entered for day.
+    while day is None: 
         day = get_day_input()
-    print(f"Thank you {inputs[0].upper()} for your inputs.\n\nThe prediction for your zodiac sign {inputs[2].upper()} for {day.upper()} is as follows.\n")
-    horoscope_text = horoscope(inputs[1], day)
+
+    print(f"Thank you {name.upper()} for your inputs.\n\nThe prediction for your zodiac sign {zodiac_2.upper()} for {day.upper()} is as follows.\n")
+    horoscope_text = horoscope(zodiac_1, day)
     print(horoscope_text)
+
 
 if __name__ == "__main__":
     main()
